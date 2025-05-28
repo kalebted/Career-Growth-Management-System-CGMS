56ded7f6ef87ef4e6873c7c624c4c82d79496e53
Kaleb Tedla <kalebttesfaye@gmail.com>
2025-05-28 12:50:05 +0300

    fixed applications imports

--- client/src/pages/applications.jsx
+++ client/src/pages/applications.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Box } from '@mui/material';
-import Sidebar from '../components/common/sidebar';
-import AppBarComponent from '../components/common/appbarcomp';
+import Sidebar from '../components/Common/sidebar';
+import AppBarComponent from '../components/Common/appbarcomp';
 import RecentApplicationHistory from '../components/Dashboard Page/recentapplicationhistory';
 
 const Applications = () => (
